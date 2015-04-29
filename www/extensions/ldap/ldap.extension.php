bc94547e6076ffc4230e4ed188ddfcc2fce6df4d
Burak Doğan <bdogan85@gmail.com>
2015-04-29 22:58:55 +0300

    Ldap Stability Update

--- www/extensions/ldap/ldap.extension.php
+++ www/extensions/ldap/ldap.extension.php
@@ -3,25 +3,28 @@
 require_once "ldap.php";
 
 function ldap_user_verify_credentials($uid, $credentials) {
-  $test_user = user_load($uid);
+  $user = user_load($uid);
   
-  if ($test_user == NULL) return false;
+  if ($user == NULL) return false;
+
+  if (isset($user["auth_method"]) && $user["auth_method"] != "ldap") return;
 
   if ($ldap = ldap_custom_connect()) {
     try {
       if (!$ldap->authenticate($uid, $credentials["pass"])) return false;
-      $users = $ldap->get_users();
-      return in_array($uid, $users);
+      return true;
     } catch (Exception $e) {
       throwError($e);
     }
   }
 
+
+
 }
 
 function ldap_custom_connect() {
   try {
-    $ldap = new LDAP(LDAP_URI, LDAP_BASE_DN, LDAP_SEARCH_DN, LDAP_BASE_DOMAIN);
+    $ldap = new LDAP(LDAP_URI, LDAP_BASE_DN, LDAP_ADMIN_DN, LDAP_ADMIN_PASSWORD);
     return $ldap;
   } catch (Exception $err) {
     throwError($err);
--- www/extensions/ldap/ldap.php
+++ www/extensions/ldap/ldap.php
@@ -3,25 +3,56 @@
 // The LDAP server
 class LDAP
 {
-    private $server = "127.0.0.1";
-    private $domain = "localhost";
-    private $admin = "admin";
-    private $password = "";
+    private $server = null;
+    private $adminDn = null;
+    private $adminPassword = null;
+    private $userPrefix = null;
     private $baseDn = "";
     private $attributes = array("cn", "memberof", "samaccountname");
-    private $baseDomain = "";
 
-    public function __construct($server, $domain, $baseDn, $baseDomain)
-    {
+    private $ldap = null;
+    private function ldapConnect(){
+        if (!empty($this->ldap)) return $this->$ldap;
+        if (!$this->serviceping($this->server, 389, 2)) return false; // Check Service if alive
+        $ldap = @ldap_connect($this->server);
+        if (!$ldap) return false;
+        $this->ldap = $ldap;
+        return $this->ldap;
+    }
+    private function bindLdap($ldap, $username = null, $password = null){
+        $username = $username ? $username : $this->adminDn;
+        $password = $password ? $password : $this->adminPassword;
+    
+        ldap_set_option($ldap, LDAP_OPT_REFERRALS, 0);
+        ldap_set_option($ldap, LDAP_OPT_PROTOCOL_VERSION, 3);
+
+        $ldapbind = @ldap_bind($ldap, $this->userPrefix . "\\" . $username, $password);
+        if ($ldapbind) return true;
+        return false;
+    }
+
+    function __construct($server, $baseDn, $adminDn, $adminPassword) {
         $this->server = $server;
-        $this->domain = $domain;
         $this->baseDn = $baseDn;
-        $this->baseDomain = $baseDomain;
+        $this->adminDn = $adminDn;
+        $this->adminPassword = $adminPassword;
+        $parts = ldap_explode_dn($this->baseDn, 1);
+        unset($parts['count']);
+        $this->userPrefix = strtoupper($parts[0]);
     }
 
     // Authenticate the against server the domain\username and password combination.
     public function authenticate($username, $password) {
-        if (!$this->serviceping($this->server, 389, 2)) return false; // Check Service if alive
+        if (($ldap = $this->ldapConnect()) == false) return false;
+
+        $result = $this->bindLdap($ldap, $username, $password);
+        ldap_close($ldap);
+        return $result;
+        if ($result === false) return false;
+
+        if (!$this->bindLdap()) return false;
+        $sr = ldap_search($this->ldap, $this->domain, "(&(sAMAccountName=" . $username . "))", $this->attributes);
+        $info = ldap_get_entries($ldap, $sr);
         
         $this->admin = $username;
         $this->password = $password;
@@ -29,11 +60,15 @@ class LDAP
         if (empty($password)) return false;
         $ldap = ldap_connect($this->server);
         if (!$ldap) return false;
-        ldap_set_option($ldap, LDAP_OPT_REFERRALS, 0);
-        ldap_set_option($ldap, LDAP_OPT_PROTOCOL_VERSION, 3);
-        $ldapbind = @ldap_bind($ldap, $this->baseDomain . "\\" . $this->admin, $this->password);
+        //ldap_set_option($ldap, LDAP_OPT_REFERRALS, 0);
+        //ldap_set_option($ldap, LDAP_OPT_PROTOCOL_VERSION, 3);
+        //$ldapbind = ldap_sasl_bind ($ldap, NULL, $password,'DIGEST-MD5',NULL,$username,NULL);
+        //print_r($ldapbind);
+        $rdn = 'OB=' . $username . ',OU=Users,' . $this->baseDn;
+        print_r($rdn);
+        $ldapbind = ldap_bind($ldap, $rdn, $password);
 
-        if($ldapbind) return true;
+        if ($ldapbind) return true;
 
         ldap_close($ldap);
         return false;
@@ -44,7 +79,7 @@ class LDAP
         if (!$op) {
             return false; //DC is N/A
         } else {
-            fclose($opanak); //explicitly close open socket connection
+            fclose($op); //explicitly close open socket connection
             return true; //DC is up & running, we can safely connect with ldap_connect
         }
     }
